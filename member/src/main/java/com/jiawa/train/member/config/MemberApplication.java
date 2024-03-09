711e523ed525d3de5b82a248fdfe17fd1bdfe7bb
yzh <1798207800@qq.com>
2024-03-09 21:06:24 +0800

    初始化

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
-package com.jiawa.train.config;
+package com.jiawa.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
+@MapperScan("com.jiawa.train.member.mapper")
 public class MemberApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
@@ -17,6 +19,6 @@ public class MemberApplication {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 }
